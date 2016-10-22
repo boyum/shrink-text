867b10f2ab23ef44df9f4752f9b247926750f0cb
Sindre <sindre.boyum@gmail.com>
2016-10-23 00:51:33 +0200

    ADD: Demo

---
+++ demo/script.js
@@ -0,0 +1,6 @@
+var nodeList = document.getElementsByClassName('shrink');
+// var nodeList = document.getElementById('shrink');
+// var nodeList = [document.getElementById('shrink')];
+
+
+shrinkText(nodeList);
