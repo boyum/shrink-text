22384dbcaa22c0bf903d70df217ed23d0ada65b0
Sindre <sindre.boyum@gmail.com>
2016-10-23 00:54:54 +0200

    FIX: Made the browser stop complaining about module not being defined. Hack from http://stackoverflow.com/a/12571962/4243638. Also added fallback for undefined config object.

--- src/shrink-text.js
+++ src/shrink-text.js
@@ -1,49 +1,46 @@
-(function() {
-  'use strict';
+'use strict';
+/**
+ * Recursively shrinks the font size of the elements given
+ * 
+ * @param {NodeList | [] | Node} elements - elements to shrink
+ * @param {Object} [config] - configuration object:
+ *  {
+ *    fontStep: Number (default: 2), - How much should be subtracted from the font size each step
+ *    lineHeightRatio: Number (default: 12/7), - Ratio between lineheight and font size
+ *    minSize: Number (default: 10) - Minimum font size
+ *  }
+ */
+function shrinkText(elements, config) {
+  elements = elements.length ? elements : [elements];
+  config = config || {};
 
-  /**
-   * Recursively shrinks the font size of the elements given
-   * 
-   * @param {NodeList | [] | Node} elements - elements to shrink
-   * @param {Object} [config] - configuration object:
-   *  {
-   *    fontStep: Number (default: 2), - How much should be subtracted from the font size each step
-   *    lineHeightRatio: Number (default: 12/7), - Ratio between lineheight and font size
-   *    minSize: Number (default: 10) - Minimum font size
-   *  }
-   */
-  function shrinkText(elements, config) {
-    elements = elements instanceof [] ? elements : [elements];
+  var fontStep = config.fontStep || 2;
+  var lineHeightRatio = config.lineHeightRatio || (12/7);
+  var minSize = config.minSize || 10;
 
-    var fontStep = config.fontStep || 2;
-    var lineHeightRatio = config.lineHeightRatio || (12/7);
-    var minSize = config.minSize || 10;
+  var fontSize;
 
-    var fontSize;
+  Array.prototype.forEach.call(elements, function(element) {
+    if (element.offsetHeight > element.parentNode.offsetHeight ||
+      element.offsetWidth > element.parentNode.offsetWidth) {
 
-    Array.prototype.forEach.call(elements, function(element) {
-      if (element.offsetHeight > element.parentNode.offsetHeight ||
-        element.offsetWidth > element.parentNode.offsetWidth) {
+      fontSize = element.style.fontSize;
 
-        fontSize = element.style.fontSize;
+      // If the font-size isn't explicitly set, element.style.fontSize returns '', which if falsy
+      if (!fontSize) {
+        fontSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+      }
+      fontSize = fontSize.substring(0, 2);
+      
+      if (fontSize > minSize) {
+        element.style.fontSize = fontSize - fontStep + 'px';
+        element.style.lineHeight = fontSize * lineHeightRatio + 'px';
 
-        // If the font-size isn't explicitly set, element.style.fontSize returns '', which if falsy
-        if (!fontSize) {
-          fontSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
-        }
-        fontSize = fontSize.substring(0, 2);
-        
-        if (fontSize > minSize) {
-          element.style.fontSize = fontSize - fontStep + 'px';
-          element.style.lineHeight = fontSize * lineHeightRatio + 'px';
-
-          if (fontSize > minSize - fontStep) {
-            shrinkText([element]);
-          }
+        if (fontSize > minSize - fontStep) {
+          shrinkText([element]);
         }
       }
-    });
-  }
-
-  module.exports = shrinkText;
-})();
+    }
+  });
+}
+(typeof module !== "undefined" && module !== null ? module : {}).exports = shrinkText;
